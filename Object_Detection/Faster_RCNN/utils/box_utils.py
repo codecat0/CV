af82f029ca3835789dec6b1b3c3556e2911f441b
codecat <2049413359@qq.com>
2021-11-11 20:40:17 +0800

    update box utils file

--- Object_Detection/Faster_RCNN/utils/box_utils.py
+++ Object_Detection/Faster_RCNN/utils/box_utils.py
@@ -10,6 +10,8 @@
 import torch
 import math
 import numpy as np
+import torch.nn.functional as F
+from torchvision.ops import nms
 
 
 def loc2bbox(src_bbox, loc, box_xform_clip=math.log(1000. / 16)):
@@ -79,3 +81,50 @@ def bbox_iou(bbox_a, bbox_b):
     area_a = np.prod(bbox_a[:, 2:] - bbox_a[:, :2], axis=1)
     area_b = np.prod(bbox_b[:, 2:] - bbox_b[:, :2], axis=1)
     return area_i / (area_a[:, None] + area_b - area_i)
+
+
+class DecodeBox(object):
+    def __init__(self, std, num_classes):
+        self.std = std
+        self.num_classes = num_classes
+
+    def forward(self, roi_cls_loc, roi_scores, rois, image_shape, input_shape, nms_iou=0.3, confidence=0.5):
+        results = []
+        batch_size = len(roi_cls_loc)
+        rois = rois.view((batch_size, -1, 4))
+        # 对每一张图片进行处理
+        for i in range(batch_size):
+            roi_cls_loc = roi_cls_loc[i] * self.std
+            roi_cls_loc = roi_cls_loc.view((-1, self.num_classes, 4))
+
+            roi = rois[i].view((-1, 1, 4)).expand_as(roi_cls_loc)
+            cls_bbox = loc2bbox(roi.contiguous().view((-1, 4)), roi_cls_loc.contiguous().view((-1, 4)))
+            cls_bbox = cls_bbox.view((-1, self.num_classes, 4))
+
+            cls_bbox[..., [0, 2]] = cls_bbox[..., [0, 2]] / input_shape[1] * image_shape[1]
+            cls_bbox[..., [1, 3]] = cls_bbox[..., [1, 3]] / input_shape[0] * image_shape[0]
+
+            roi_score = roi_scores[i]
+            prob = F.softmax(roi_score, dim=-1)
+
+            results.append([])
+            for c in range(1, self.num_classes):
+                c_confs = prob[:, c]
+                c_confs_m = c_confs > confidence
+
+                if len(c_confs[c_confs_m]) > 0:
+                    boxes_to_process = cls_bbox[c_confs_m, c]
+                    confs_to_process = c_confs[c_confs_m]
+
+                    keep = nms(
+                        boxes_to_process,
+                        confs_to_process,
+                        nms_iou
+                    )
+
+                    good_boxes = boxes_to_process[keep]
+                    confs = confs_to_process[keep][:, None]
+                    labels = (c - 1) * torch.ones((len(keep), 1)).cuda() if confs.is_cuda else (c-1) * torch.ones((len(keep), 1))
+
+                    c_pred = torch.cat((good_boxes, confs, labels), dim=1).cpu().numpy()
+                    results[-1].extend(c_pred)
