c6d6957b91018ae0f5cfd609b8fda40d53fa4c59
codecat <2049413359@qq.com>
2021-11-11 16:04:31 +0800

    update train_utils file

--- Object_Detection/Faster_RCNN/utils/train_utils.py
+++ Object_Detection/Faster_RCNN/utils/train_utils.py
@@ -82,3 +82,22 @@ def train_one_epoch(model, trainer, loss_history, optimizer, epoch, epoch_step, 
         print('Epoch:' + str(epoch+1) + '/' + str(sum_epoch))
         print('Total Loss: %.3f || Val loss: %.3f' % (total_loss / epoch_step, val_loss /epoch_step))
         torch.save(model.state_dict(), 'logs/ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch+1, total_loss/epoch_step, val_loss/epoch_step))
+
+
+def weight_init(net, init_type='normal', init_gain=0.02):
+    def init_func(m):
+        class_name = m.__class__.__name__
+        if hasattr(m, 'weight') and class_name.find('Conv') != -1:
+            if init_type == 'normal':
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            else:
+                raise ValueError("initialization method [%s] is not implement" % init_type)
+        elif class_name.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    print('initialize network with %s type' % init_type)
+    net.apply(init_func)
