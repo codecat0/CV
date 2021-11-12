1f8faf75f72c8ff608420e551183d333e7e54959
codecat <2049413359@qq.com>
2021-11-12 16:32:34 +0800

    update metric utils file

--- Object_Detection/Faster_RCNN/utils/metric_utils.py
+++ Object_Detection/Faster_RCNN/utils/metric_utils.py
@@ -147,3 +147,69 @@ def adjust_axes(r, t, fig, axes):
     axes.set_xlim(x_lim[0], x_lim[1] * propotion)
 
 
+def draw_plot_func(dictionary, n_classes, window_title, plot_title, x_label, output_path, to_show, plot_color, true_p_bar):
+    """
+    Draw plot using Matplotlib
+    """
+    sorted_dict_by_value = sorted(dictionary.items(), key=operator.itemgetter(1))
+    sorted_keys, sorted_values = zip(*sorted_dict_by_value)
+
+    if true_p_bar != "":
+        fp_sorted = []
+        tp_sorted = []
+        for key in sorted_keys:
+            fp_sorted.append(dictionary[key] - true_p_bar[key])
+            tp_sorted.append(true_p_bar[key])
+        plt.barh(range(n_classes), fp_sorted, align='center', color='crimson', label='False Positive')
+        plt.barh(range(n_classes), tp_sorted, align='center', color='forestgreen', label='True Positive', left=fp_sorted)
+
+        plt.legend()
+
+        fig = plt.gcf()
+        axes = plt.gca()
+        r = fig.canvas.get_renderer()
+        for i, val in enumerate(sorted_values):
+            fp_val = fp_sorted[i]
+            tp_val = tp_sorted[i]
+            fp_str_val = " " + str(fp_val)
+            tp_str_val = fp_str_val + " " + str(tp_val)
+            t = plt.text(val, i, tp_str_val, color='forestgreen', va='center', fontweight='bold')
+            plt.text(val, i, fp_str_val, color='crimson', va='center', fontweight='bold')
+            if (i + 1) == len(sorted_values):
+                adjust_axes(r, t, fig, axes)
+    else:
+        plt.barh(range(n_classes), sorted_values, color=plot_color)
+        fig = plt.gcf()
+        axes = plt.gca()
+        r = fig.canvas.get_renderer()
+        for i, val in enumerate(sorted_values):
+            str_val = " " + str(val)
+            if val < 1.0:
+                str_val = " {:.2f}".format(val)
+            t = plt.text(val, i, str_val, color=plot_color, va='center', fontweight='bold')
+            if (i+1) == len(sorted_values):
+                adjust_axes(r, t, fig, axes)
+
+    fig.canvas.set_window_title(window_title)
+    tick_font_size = 12
+    plt.yticks(range(n_classes), sorted_keys, fontsize=tick_font_size)
+
+    init_height = fig.get_figheight()
+    dpi = fig.dpi
+    height_pt = n_classes * (tick_font_size * 1.4)
+    height_inches = height_pt / dpi
+    top_margin = 0.15
+    bottom_margin = 0.05
+    figure_height = height_inches / (1 - top_margin - bottom_margin)
+    if figure_height > init_height:
+        fig.set_figheight(figure_height)
+
+    plt.title(plot_title, fonsize=14)
+    plt.xlabel(x_label, fontsize='large')
+    fig.tight_layout()
+    fig.savefig(output_path)
+    if to_show:
+        plt.show()
+    plt.close()
+
+
