19f61ef1615ad5bd5ec499abe4ee9a251a657eae
codecat <2049413359@qq.com>
2021-11-16 10:54:18 +0800

    update ssd dataset file

--- Object_Detection/SSD/dataset/ssd_dataset.py
+++ Object_Detection/SSD/dataset/ssd_dataset.py
@@ -38,6 +38,23 @@ class SSDDataset(Dataset):
     def __len__(self):
         return len(self.annotation_lines)
 
+    def __getitem__(self, index):
+        index = index % len(self.annotation_lines)
+        image, box = self.get_random_data(self.annotation_lines[index], self.input_shape, is_image_enhance=self.train)
+        image_data = np.transpose(preprocess_input(np.array(image, dtype=np.float32)), (2, 0, 1))
+        if len(box) != 0:
+            boxes = np.array(box[:, :4], dtype=np.float32)
+            # 归一化处理
+            boxes[:, [0, 2]] /= self.input_shape[1]
+            boxes[:, [1, 3]] /= self.input_shape[0]
+            # 对真实框的种类进行one hot处理
+            one_hot_label = np.eye(self.num_anchors-1)[np.array(box[:, 4], np.int32)]
+            box = np.concatenate([boxes, one_hot_label], axis=1)
+
+        box = self.assigin_boxer(boxes)
+        return np.array(image_data), np.array(box)
+
+
     @staticmethod
     def rand(a=0, b=1):
         return np.random.rand()*(b - a) + a
@@ -50,42 +67,43 @@ class SSDDataset(Dataset):
             hue=.1,
             sat=1.5,
             val=1.5,
-            random=True
+            is_image_enhance=True
     ):
         line = annotation_line.split()
-        # 读取图像并转换成RGB图像
+
         image = Image.open(line[0])
         image = cvtColor(image)
-        # 获得图像的高宽与目标高宽
-        iw, ih = image.size
+
+        img_w, img_h = image.size
         h, w = input_shape
-        # 获得真实边界框及标签
+
         box = np.array([np.array(list(map(int, box.split(',')))) for box in line[1:]])
 
-        if not random:
-            scale = min(w/iw, h/ih)
-            nw = int(iw*scale)
-            nh = int(ih*scale)
-            dx = (w-nw)//2
-            dy = (h-nh)//2
+        if not is_image_enhance:
+            scale = min(w / img_w, h / img_h)
+            new_w = int(img_w * scale)
+            new_h = int(img_h * scale)
+            dx = (w - new_w) // 2
+            dy = (h - new_h) // 2
 
-            image = image.resize((nw, nh), Image.BICUBIC)
-            new_image = Image.new('RGB', (w, h), (128, 128, 128))
+            image = image.resize((new_w, new_h), Image.BICUBIC)
+            new_image = Image.new(mode='RGB', size=(w, h), color=(128, 128, 128))
             new_image.paste(image, (dx, dy))
             image_data = np.array(new_image, np.float32)
 
             if len(box) > 0:
                 np.random.shuffle(box)
-                box[:, [0, 2]] = box[:, [0, 2]] * nw / iw + dx
-                box[:, [1, 3]] = box[:, [1, 3]] * nh / ih + dy
+                box[:, [0, 2]] = box[:, [0, 2]] * new_w / img_w + dx
+                box[:, [1, 3]] = box[:, [1, 3]] * new_h / img_h + dy
                 box[:, 0:2][box[:, 0:2] < 0] = 0
                 box[:, 2][box[:, 2] > w] = w
                 box[:, 3][box[:, 3] > h] = h
-                bow_w = box[:, 2] - box[:, 0]
+                box_w = box[:, 2] - box[:, 0]
                 box_h = box[:, 3] - box[:, 1]
-                box = box[np.logical_and(box_h>1, bow_w>1)]
+                box = box[np.logical_and(box_w > 1, box_h > 1)]
 
             return image_data, box
+
         # 对图像进行缩放并且进行长和宽的扭曲
         new_ar = w / h + self.rand(1 - jitter, 1 + jitter) / self.rand(1 - jitter, 1 + jitter)
         scale = self.rand(0.25, 2)
@@ -209,3 +227,32 @@ class SSDDataset(Dataset):
         encoded_boxes = np.apply_along_axis(self.encode_box, 1, boxes[:, :4])
         # reshape后，获得的encoded_boxes的shape为：(num_true_box, num_anchors, 4+1)，4对应回归参数、1为iou
         encoded_boxes = encoded_boxes.reshape(-1, self.num_anchors, 5)
+
+        # 求取每一个先验框重合度最大的真实框
+        # best_iou : (num_anchors,) 其值为anchor与box的最大iou值
+        best_iou = encoded_boxes[:, :, -1].max(axis=0)
+        # best_iou_idx: (num_anchors,) 其值为anchor与box最大iou值box的索引
+        best_iou_idx = encoded_boxes[:, :, -1].argmax(axis=0)
+        # best_iou_idx: (num_anchors,) 其值是bool，最大iou值大于0为True，反之为False
+        best_iou_mask = best_iou > 0
+        best_iou_idx = best_iou_idx[best_iou_mask]
+
+        assign_num = len(best_iou_idx)
+        encoded_boxes = encoded_boxes[:, best_iou_mask, :]
+        assignment[:, :4][best_iou_mask] = encoded_boxes[best_iou_idx, np.arange(assign_num), :4]
+
+        assignment[:, 4][best_iou_mask] = 0
+        assignment[:, 5:-1][best_iou_mask] = boxes[best_iou_idx, 4:]
+        assignment[:, -1][best_iou_mask] = 1
+        return assignment
+
+
+def ssd_dataset_collate(batch):
+    images = []
+    bboxes = []
+    for img, box in batch:
+        images.append(img)
+        bboxes.append(box)
+    images = np.array(images)
+    bboxes = np.array(bboxes)
+    return images, bboxes
