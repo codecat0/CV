19236d51cac3b5c630649b8742db98b9503d1e03
codecat <2049413359@qq.com>
2021-12-31 17:03:43 +0800

    添加FCN网络的主干特征提取网络resnet34

---
+++ Semantic_Segmentation/FCN/nets/resnet.py
@@ -0,0 +1,106 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : resnet.py
+# Author     ：CodeCat
+# version    ：python 3.7
+# Software   ：Pycharm
+"""
+import torch
+import torch.nn as nn
+from torch.hub import load_state_dict_from_url
+
+
+model_urls = {
+    'resnet34': 'https://download.pytorch.org/models/resnet34-b627a593.pth'
+}
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identify = x
+        if self.downsample is not None:
+            identify = self.downsample(x)
+
+        out = self.relu(self.bn1(self.conv1(x)))
+
+        out = self.bn2(self.conv2(out))
+
+        out += identify
+        out = self.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, block_nums, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, block_nums[0])
+        self.layer2 = self._make_layer(block, 128, block_nums[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, block_nums[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, block_nums[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, channels, block_num, stride=1):
+        downsample = None
+
+        if stride != 1 or self.in_channels != channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, channels * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channels * block.expansion)
+            )
+
+        layers = []
+
+        layers.append(block(self.in_channels, channels, stride=stride, downsample=downsample))
+        self.in_channels = channels * block.expansion
+        for _ in range(1, block_num):
+            layers.append(block(self.in_channels, channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc(x)
+
+        return x
+
+
+def resent34(num_classes=1000, pretrained=False):
+    model = ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls['resnet34'], progress=True)
+        model.load_state_dict(state_dict)
+    return model
