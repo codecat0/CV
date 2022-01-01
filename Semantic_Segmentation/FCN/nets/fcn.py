5a0134b0adf324ce0c889f7f1df4da328e212158
codecat <2049413359@qq.com>
2022-01-01 15:20:03 +0800

    添加FCN网络模型初始化权重设置

--- Semantic_Segmentation/FCN/nets/fcn.py
+++ Semantic_Segmentation/FCN/nets/fcn.py
@@ -101,6 +101,27 @@ class FCN(nn.Module):
         return out
 
 
+def weights_init(net, init_type='normal', init_gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and classname.find('Conv') != -1:
+            if init_type == 'normal':
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                torch.nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    print('initialize network with %s type' % init_type)
+    net.apply(init_func)
+
+
 if __name__ == '__main__':
     inputs = torch.randn(1, 3, 512, 512)
     model = FCN()
