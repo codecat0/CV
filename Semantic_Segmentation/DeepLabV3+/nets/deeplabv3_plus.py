15b0ea205a47b5db91b2a6e822127631104f93b7
codecat <2049413359@qq.com>
2022-04-30 16:20:30 +0800

    添加DeepLabV3+网络

---
+++ Semantic_Segmentation/DeepLabV3+/nets/deeplabv3_plus.py
@@ -0,0 +1,178 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : deeplabv3_plus.py
+# Author     ：CodeCat
+# version    ：python 3.7
+# Software   ：Pycharm
+"""
+from functools import partial
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from mobilenetv2 import mobilenetv2
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, downsample_factor=8, pretrained=True):
+        super(MobileNetV2, self).__init__()
+
+        model = mobilenetv2(pretrained)
+        self.features = model.features[:-1]
+
+        self.total_idx = len(self.features)
+        self.down_idx = [2, 4, 7, 14]
+
+        if downsample_factor == 8:
+            for i in range(self.down_idx[-2], self.down_idx[-1]):
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=2)
+                )
+            for i in range(self.down_idx[-1], self.total_idx):
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=4)
+                )
+        elif downsample_factor == 16:
+            for i in range(self.down_idx[-1], self.total_idx):
+                self.features[i].apply(
+                    partial(self._nostride_dilate, dilate=2)
+                )
+        else:
+            raise ValueError(
+                'downsample_factor is {}, but must be one of 8 and 16'.format(downsample_factor)
+            )
+
+    @staticmethod
+    def _nostride_dilate(m, dilate):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            if m.stride == (2, 2):
+                m.stride = (1, 1)
+                if m.kernel_size == (3, 3):
+                    m.dilation = (dilate // 2, dilate // 2)
+                    m.padding = (dilate // 2, dilate // 2)
+            else:
+                if m.kernel_size == (3, 3):
+                    m.dilation = (dilate, dilate)
+                    m.padding = (dilate, dilate)
+
+    def forward(self, x):
+        low_level_features = self.features[:4](x)
+        x = self.features[4:](low_level_features)
+        return low_level_features, x
+
+
+class ASPP(nn.Module):
+    def __init__(self, in_channels, out_channels, rate=1, bn_momentum=0.1):
+        super(ASPP, self).__init__()
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=1, padding=0, dilation=rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=1, padding=6*rate, dilation=6*rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch3 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=1, padding=12*rate, dilation=12*rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch4 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=1, padding=18*rate, dilation=18*rate),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+        self.branch5_conv = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=1, padding=0, bias=False)
+        self.branch5_bn = nn.BatchNorm2d(out_channels, momentum=bn_momentum)
+        self.branch5_relu = nn.ReLU(inplace=True)
+
+        self.conv_cat = nn.Sequential(
+            nn.Conv2d(out_channels*5, out_channels, kernel_size=(1, 1), stride=1, padding=0),
+            nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        conv1x1 = self.branch1(x)
+        conv3x3_1 = self.branch2(x)
+        conv3x3_2 = self.branch3(x)
+        conv3x3_3 = self.branch4(x)
+
+        global_feature = torch.mean(x, dim=2, keepdim=True)
+        global_feature = torch.mean(global_feature, dim=3, keepdim=True)
+        global_feature = self.branch5_conv(global_feature)
+        global_feature = self.branch5_bn(global_feature)
+        global_feature = self.branch5_relu(global_feature)
+        global_feature = F.interpolate(global_feature, size=(h, w), mode='bilinear', align_corners=True)
+
+        feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
+        result = self.conv_cat(feature_cat)
+        return result
+
+
+class DeepLab(nn.Module):
+    def __init__(self, num_classes, backbone='mobilenet', pretrained=True, downsample_factor=16):
+        super(DeepLab, self).__init__()
+        if backbone == 'mobilenet':
+            self.backbone = MobileNetV2(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 320
+            low_level_channels = 24
+        else:
+            raise ValueError(
+                'Unsuppored backbone - {}, Use mobilenet'.format(backbone)
+            )
+
+        self.aspp = ASPP(
+            in_channels=in_channels,
+            out_channels=256,
+            rate=16 // downsample_factor
+        )
+
+        self.shortcut_conv = nn.Sequential(
+            nn.Conv2d(low_level_channels, 48, kernel_size=(1, 1)),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True)
+        )
+
+        self.cat_conv = nn.Sequential(
+            nn.Conv2d(48+256, 256, kernel_size=(3, 3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1)
+        )
+        self.cls_conv = nn.Conv2d(256, num_classes, kernel_size=(1, 1))
+
+    def forward(self, x):
+        H, W = x.size(2), x.size(3)
+
+        low_level_feature, x = self.backbone(x)
+        x = self.aspp(x)
+        low_level_feature = self.shortcut_conv(low_level_feature)
+
+        x = F.interpolate(x, size=(low_level_feature.size(2), low_level_feature.size(3)), mode='bilinear', align_corners=True)
+        x = self.cat_conv(torch.cat((x, low_level_feature), dim=1))
+        x = self.cls_conv(x)
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+        return x
+
+
+if __name__ == '__main__':
+    model = DeepLab(
+        num_classes=21,
+        pretrained=False
+    )
+    inputs = torch.randn(size=(2, 3, 512, 512))
+    outputs = model(inputs)
+    print(outputs.shape)
