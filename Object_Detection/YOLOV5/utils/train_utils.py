2d6aa91e7acebf18c043215267ab38684da54989
codecat <2049413359@qq.com>
2022-06-07 16:59:29 +0800

    添加YOLOV5训练时学习率如何设置

--- Object_Detection/YOLOV5/utils/train_utils.py
+++ Object_Detection/YOLOV5/utils/train_utils.py
@@ -7,8 +7,49 @@
 # version    ：python 3.7
 # Software   ：Pycharm
 """
+import math
+from functools import partial
 import torch
 from tqdm import tqdm
+
+
+def get_lr_scheduler(lr_decay_type, lr, min_lr, total_iters, warmup_iters_ratio=0.1, warmup_lr_ratio=0.1,
+                     no_aug_iter_ratio=0.3, step_num=10):
+    def warm_cos_lr(lr, min_lr, total_iters, warmup_total_iters, warmup_lr_start, no_aug_iter, iters):
+        if iters <= warmup_total_iters:
+            out_lr = (lr - warmup_lr_start) * pow(iters / float(warmup_total_iters), 2)
+        elif iters >= total_iters - no_aug_iter:
+            out_lr = min_lr
+        else:
+            out_lr = min_lr + 0.5 * (lr - min_lr) * (
+                1.0 + math.cos(math.pi * (iters - warmup_total_iters) / (total_iters - warmup_total_iters - no_aug_iter))
+            )
+        return out_lr
+
+    def step_lr(lr, decay_rate, step_size, iters):
+        if step_size < 1:
+            raise ValueError('step_size must above 1.')
+        n = iters // step_size
+        out_lr = lr * decay_rate ** n
+        return out_lr
+
+    if lr_decay_type == 'cos':
+        warmup_total_iters = min(max(warmup_iters_ratio * total_iters, 1), 3)
+        warmup_lr_start = max(warmup_lr_ratio * lr, 1e-6)
+        no_aug_iter = min(max(no_aug_iter_ratio * total_iters, 1), 15)
+        func = partial(warm_cos_lr, lr, min_lr, total_iters, warmup_total_iters, warmup_lr_start, no_aug_iter)
+    else:
+        deacy_rate = (min_lr / lr) ** (1 / (step_num - 1))
+        step_size = total_iters / step_num
+        func = partial(step_lr, lr, deacy_rate, step_size)
+
+    return func
+
+
+def set_optimizer_lr(optimizer, lr_scheduler_func, epoch):
+    lr = lr_scheduler_func(epoch)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
 
 def get_lr(optimizer):
