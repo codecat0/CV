9503e5c484203ff1c862f90364c9da79110fd2aa
codecat <2049413359@qq.com>
2022-06-07 15:51:59 +0800

    添加YOLOV5训练时网络权重初始化

--- Object_Detection/YOLOV5/utils/loss_utils.py
+++ Object_Detection/YOLOV5/utils/loss_utils.py
@@ -173,3 +173,25 @@ class YOLOLoss(nn.Module):
         pred_boxes_h = torch.unsqueeze((h * 2) ** 2 * anchor_h, -1)
         pred_boxes = torch.cat([pred_boxes_x, pred_boxes_y, pred_boxes_w, pred_boxes_h], dim=-1)
         return pred_boxes
+
+
+def weights_init(net, init_type='normal', init_gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and classname.find('Conv') != -1:
+            if init_type == 'normal':
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                torch.nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    print('initialize network with %s type' % init_type)
+    net.apply(init_func)
+
